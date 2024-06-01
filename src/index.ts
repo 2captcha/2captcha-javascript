121da95250571d0a5ddb64d4e4218abdfd41208c
Alexander Roidl <alexanderroidl@gmail.com>
2024-06-01 19:28:14 +0200

    refactor: Moved Solver main class to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { Solver } from "./structs/2captcha"
+export { Solver } from "./structs/2captchaSolver"
 export { APIError } from "./structs/2captchaError"
 // export { Server } from "./structs/2captchaServer.js"
